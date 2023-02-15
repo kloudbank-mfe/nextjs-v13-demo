d5922c3286009aabab16ea62823f19269e541d7f
noisonnoiton <noisonnoiton@gmail.com>
2023-02-15 10:49:52 +0900

    add antd lib., custom table components

---
+++ host/pages/pagination/index.js
@@ -0,0 +1,10 @@
+import { Layout } from 'antd';
+import CustomTable from '#/components/CustomTable';
+
+export default function Pagination() {
+  return (
+      <Layout className="layout">
+        <CustomTable />
+      </Layout>
+  )
+}
